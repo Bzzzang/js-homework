ddad6cf3fc60a3bbe330e92890bec035ddab4612
Bzzzang <sindmsql@gmail.com>
2024-11-23 12:59:26 +0900

    포스터 UI

---
+++ mission01/naver_login/js/main.js
@@ -0,0 +1,45 @@
+
+const user = {
+  id:'asd@naver.com',
+  pw:'spdlqj123!@'
+}
+
+/*
+
+1. email 정규표현식을 사용한 validation
+2. pw 정규표현식을 사용한 validation
+3. 상태 변수 관리
+4. 로그인 버튼을 클릭시 조건처리
+
+*/
+
+function emailReg(text){
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(text).toLowerCase())
+}
+
+function pwReg(text){
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  return re.test(String(text).toLowerCase());
+}
+
+function getNode(node) {
+  if (typeof node === 'string') node = document.querySelector(node);
+  if (node.nodeType === 1) {
+    return node;
+  } else {
+    throw new TypeError(`${node}는 문자열이 아니거나 node가 아닙니다.`);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
